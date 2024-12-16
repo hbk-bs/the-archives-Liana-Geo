c647449713a4c241f00cb4fc268304a730cf18fc
Liana-Geo <l.geonjyan@hbk-bs.de>
2024-12-16 14:38:46 +0100

    r

--- into-light/index.js
+++ into-light/index.js
@@ -1,65 +1,63 @@
-let bigParticle; 
-let smallParticle; 
-let angle = 0; 
-let orbitRadius = 50; 
-let speed = 2; 
+
+class Particle {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.vel = p5.Vector.random2D();
+    this.speed = random(1, 3);
+    this.vel.setMag(this.speed);
+    this.target = createVector(width / 2, height / 2); 
+  }
+
+  move() {
+    let force = p5.Vector.sub(this.target, this.pos);
+    force.setMag(0.05);
+    this.vel.add(force);
+    this.vel.limit(this.speed);
+    this.pos.add(this.vel);
+    let squareSize = 200;
+    let halfSize = squareSize / 2;
+    let left = width / 2 - halfSize;
+    let right = width / 2 + halfSize;
+    let top = height / 2 - halfSize;
+    let bottom = height / 2 + halfSize;
+
+    if (this.pos.x > left && this.pos.x < right && this.pos.y > top && this.pos.y < bottom) {
+      if (this.pos.x <= left || this.pos.x >= right) {
+        this.vel.x *= -1;
+      }
+      if (this.pos.y <= top || this.pos.y >= bottom) {
+        this.vel.y *= -1;
+      }
+    }
+  }
+
+  display() {
+    fill(255);
+    noStroke();
+    ellipse(this.pos.x, this.pos.y, 5);
+  }
+}
+
+let particles = [];
 
 function setup() {
-  createCanvas(800, 400);
-  bigParticle = new Particle(50, height / 2, 30); 
-  smallParticle = new Satellite(bigParticle, orbitRadius, 10); 
+  createCanvas(600, 600);
+  for (let i = 0; i < 100; i++) {
+    particles.push(new Particle(random(width), random(height)));
+  }
 }
 
 function draw() {
   background(30);
-  
-  
-  bigParticle.update();
-  bigParticle.show();
-  
-  smallParticle.update();
-  smallParticle.show();
-}
 
-class Particle {
-  constructor(x, y, size) {
-    this.x = x;
-    this.y = y;
-    this.size = size;
-  }
-  
-  update() {
-    this.x += speed; 
-    if (this.x - this.size / 2 > width) {
-      this.x = -this.size / 2;
-    }
-  }
-  
-  show() {
-    fill(255);
-    noStroke();
-    ellipse(this.x, this.y, this.size);
-  }
-}
+  let squareSize = 200;
+  fill("yellow");
+  noStroke();
+  rectMode(CENTER);
+  rect(width / 2, height / 2, squareSize, squareSize);
 
-class Satellite {
-  constructor(centerParticle, orbitRadius, size) {
-    this.centerParticle = centerParticle;
-    this.orbitRadius = orbitRadius;
-    this.size = size;
-    this.angle = 0; 
-  }
-  
-  update() {
-    this.angle += 0.05; 
-  }
-  
-  show() {
-    let x = this.centerParticle.x + cos(this.angle) * this.orbitRadius;
-    let y = this.centerParticle.y + sin(this.angle) * this.orbitRadius;
-    
-    fill(150, 200, 255);
-    noStroke();
-    ellipse(x, y, this.size);
+  for (let particle of particles) {
+    particle.move();
+    particle.display();
   }
 }
